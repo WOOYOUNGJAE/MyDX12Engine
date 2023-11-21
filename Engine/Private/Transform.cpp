eec73f8f9994917db3be2b3573cb3c31b4a82002
WOOYOUNGJAE <76108701+WOOYOUNGJAE@users.noreply.github.com>
2023-11-21 12:33:51 +0900

    Transform 진행중, Transform연산 구현해야

--- Engine/Private/Transform.cpp
+++ Engine/Private/Transform.cpp
@@ -1,1 +1,28 @@
 #include "Transform.h"
+
+void CTransform::Set_WorldMatrix(MATRIX_ENUM eEnum, _fvector vVec)
+{
+	_matrix		WorldMatrix = XMLoadFloat4x4(&m_WorldMatrix);
+
+	WorldMatrix.r[eEnum] = vVec;
+
+	XMStoreFloat4x4(&m_WorldMatrix, WorldMatrix);
+}
+
+void CTransform::Refresh_Transform(TRANSFORM_ENUM eEnum)
+{
+	switch (eEnum)
+	{
+	case LOOK:
+		{
+			_vector vRight = XMVector3Cross(XMVectorSet(0.f, 1.f, 0.f, 0.f), XMLoadFloat3(&m_Look));
+		}
+		break;
+	case POSITION:
+		break;
+	case SCALE:
+		break;
+	default:
+		break;
+	}
+}
--- Engine/Public/Transform.h
+++ Engine/Public/Transform.h
@@ -3,8 +3,14 @@
 
 NAMESPACE_(Engine)
 
+// Client에게 TransformOperation (Look, Position, Scale) 클래스를 통해 접근 허용,
+// TransformOperation을 통해 정보가 바뀌면 자동으로 CTransform의 Refresh를 통해
+// CTransform의 멤버변수를 바꾸고 그에 맞게 월드매트릭스도 업데이트
 class ENGINE_DLL CTransform final : public CComponent
 {
+public: // typedef
+	enum TRANSFORM_ENUM {LOOK, POSITION, SCALE, TRANSFORM_ENUM_END,};
+	enum MATRIX_ENUM { MAT_RIGHT, MAT_UP, MAT_LOOK, MAT_POSITION, MATRIX_ENUM_END };
 private:
 	CTransform() = default;
 	CTransform(CTransform& rhs) = default;
@@ -12,8 +18,69 @@ private:
 
 public:
 	CComponent* Clone(void* pArg) override;
+
+public: // getter setter
+	_float4x4 WorldMatrix() { return m_WorldMatrix; }
+	_float3 Position() { return m_Position; }
+	_float3 Right() { return m_Right; }
+	_float3 Up() { return m_Up; }
+	_float3 Look() { return m_Look; }
+	_float3 Set_Look();
+	_float3 Set_Scale();
+	void Set_WorldMatrix(const _float4x4& matrix) { m_WorldMatrix = matrix; }
+	void Set_WorldMatrix(MATRIX_ENUM eEnum, _fvector vVec);
+	
+	void Set_Position(const _float3& pos) { m_Position = pos; }
+	
+public:
+	void Refresh_Transform(TRANSFORM_ENUM eEnum = TRANSFORM_ENUM_END);
+
+public:
+	_float3 m_Position;
+
 private:
 	_float4x4 m_WorldMatrix;
+	_float3 m_Scale;
+	_float3 m_Right;
+	_float3 m_Up;
+	_float3 m_Look;
+
+};
+
+// 외부에서 직접적으로 포지션 값을 변경하는 동시에 트랜스폼의 월드 매트릭스도 변경하게 끔
+class Position
+{
+public:
+	Position() = default;
+	~Position()
+	{	
+		Safe_Release(m_pOwner);
+	}
+
+public: // Operator Overload
+	_float3 operator+(_vector vVector)
+	{
+		_vector vPos = XMLoadFloat3(&m_Position);
+
+		vPos = XMVectorAdd(vPos, vVector);
+		XMStoreFloat3(&m_Position, vPos);
+		m_pOwner->Set_Position(m_Position);
+		m_pOwner->Refresh_Transform();
+		return m_Position;
+	}
+	_float3 operator-(_vector vVector)
+	{
+		_vector vPos = XMLoadFloat3(&m_Position);
+
+		vPos = XMVectorAdd(vPos, -vVector);
+		XMStoreFloat3(&m_Position, vPos);
+		return m_Position;
+	}
+public:
+	void Set_Owner(CTransform* pOwner) { m_pOwner = pOwner; Safe_AddRef(m_pOwner); }
+private:
+	_float3 m_Position;
+	CTransform* m_pOwner = nullptr;
 };
 
 _NAMESPACE
